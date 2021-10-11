d53e54bd0703eca6a84519e4f0e350770ea296dd
Ézsely Szovát <ezselyszovat@gmail.com>
2021-10-11 18:18:30 +0200

    backend which complies

---
+++ meetonefriender/src/main/java/elte/meetonefriender/MeetonefrienderApplication.java
@@ -0,0 +1,13 @@
+package elte.meetonefriender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetonefrienderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetonefrienderApplication.class, args);
+	}
+
+}
---
+++ meetonefriender/src/test/java/elte/meetonefriender/MeetonefrienderApplicationTests.java
@@ -0,0 +1,13 @@
+package elte.meetonefriender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetonefrienderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
